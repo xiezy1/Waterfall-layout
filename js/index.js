9fce6255569acc9606a2e5c2aa7c374bf9212f11
xiezy <2973912389@qq.com>
2022-06-03 08:23:20 +0800

    'first commit'

---
+++ js/index.js
@@ -0,0 +1,44 @@
+class Wallpaper {
+    constructor({ el, col, itemEl, gutter }) {
+        this.el = el && document.querySelector(el)
+        this.col = col
+        this._arr = []
+        this.gutter = gutter
+        this.imageList = itemEl && document.querySelectorAll(itemEl)
+        this.imageListLength = this.imageList.length
+        this.imageWidth = (this.el.offsetWidth - (this.col - 1) * this.gutter) / this.col
+        this.init()
+    }
+    init() {
+        this.setImgPosition()
+    }
+
+    getMinHeight(value) {
+        let _arr = value.map(item => item.height)
+        let minValue = Math.min(..._arr)
+        return _arr.findIndex(i => i == minValue)
+    }
+
+    setImgPosition() {
+        for (let i = 0; i < this.imageListLength; i++) {
+            let item = this.imageList[i]
+            item.style.width = this.imageWidth + 'px'
+            if (i < this.col) {
+                item.style.top = '0px'
+                let itemLeft = i * item.offsetWidth + i * this.gutter
+                item.style.left = itemLeft + 'px'
+                this._arr.push({
+                    height: item.offsetHeight + this.gutter,
+                    left: itemLeft
+                })
+            } else {
+                let minIndex = this.getMinHeight(this._arr)
+                let { height, left } = this._arr[minIndex]
+                this._arr[minIndex] = { height: item.offsetHeight + height + this.gutter, left }
+                item.style.top = height + 'px'
+                item.style.left = left + 'px'
+            }
+        }
+    }
+
+}
